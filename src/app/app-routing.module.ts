a2185858a6e23de5620468400ea4faf2d36205a0
dualipin <118947989+dualipin@users.noreply.github.com>
2023-08-08 13:47:12 -0600

    update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { MyLikesComponent } from './pages/my-likes/my-likes.component';
+import { ContactMeComponent } from './pages/contact-me/contact-me.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/', redirectTo: 'home', pathMatch: 'full',
+    component: HomeComponent
+  },
+  {
+    path: 'my-likes', component: MyLikesComponent
+  },
+  {
+    path: 'contact-me', component: ContactMeComponent
+  },
+  {
+    path: '**', redirectTo: 'home', pathMatch: 'full',
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
