ebbbf2c73342b4a36f06b69455ee290f118e90af
dualipin <118947989+dualipin@users.noreply.github.com>
2023-08-08 13:22:11 -0600

    create page my likes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './pages/home/home.component';
+import { MyLikesComponent } from './pages/my-likes/my-likes.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    MyLikesComponent
   ],
   imports: [
     BrowserModule,
